1792a4188beaaaabad112f439d4b3d0fac17cf09
Kabiru Sani <kabirusani007@gmail.com>
2026-01-29 16:49:16 +0100

    staff loging fix

--- resources/views/livewire/admin/includes/navbar.blade.php
+++ resources/views/livewire/admin/includes/navbar.blade.php
@@ -29,17 +29,18 @@
                         <span>Dashboard</span>
                     </a>
                 </li>
-
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="fa fa-users"></i>
-                        <span>Team Members</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="{{ route('team.create') }}">Add Team Member</a></li>
-                        <li><a href="{{ route('team.index') }}">Manage Team Members</a></li>
-                    </ul>
-                </li>
+                @if (Auth::user()->user_type === 'Admin')
+                    <li>
+                        <a href="javascript: void(0);" class="has-arrow waves-effect">
+                            <i class="fa fa-users"></i>
+                            <span>Team Members</span>
+                        </a>
+                        <ul class="sub-menu" aria-expanded="false">
+                            <li><a href="{{ route('team.create') }}">Add Team Member</a></li>
+                            <li><a href="{{ route('team.index') }}">Manage Team Members</a></li>
+                        </ul>
+                    </li>
+                @endif
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
                         <i class="mdi mdi-settings"></i>
